d5d5728aaf6cf9d8ad1cb2f653fac23b1046c422
Andrea Cox <msandrea712@yahoo.com>
2023-08-11 12:15:32 -0400

    hook state events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState([]);
+  const updateCart = (name) => {
+    const existingItem = cart.find((val) => val === name);
+    if (!existingItem) {
+      setCart((prevState) => [...prevState, name]);
+    } else {
+      setCart((prevState) => prevState.filter((item) => item !== name));
+    }
+  };
+
+  if (cart.indexOf(name) > -1) {
+    return (
+      <li className="in-cart">
+        <span>{name}</span>
+        <span className="category">{category}</span>
+        <button className="add" onClick={() => updateCart(name)}>
+          Remove From Cart
+        </button>
+      </li>
+    );
+  }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => updateCart(name)}>
+        Add to Cart
+      </button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [currentItems, setCurrentItems] = useState(items);
+
+  const updateCategory = (category) => {
+    switch (category) {
+      case "All":
+        setCurrentItems(items);
+        break;
+      default:
+        setCurrentItems(items.filter((val) => val.category === category));
+    }
+  };
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select
+          name="filter"
+          onChange={(e) => {
+            updateCategory(e.target.value);
+          }}
+        >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +30,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {currentItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +39,5 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
+
